7aa3991a2cb970690be5e3a7cf198873a8c6881b
thelinuxguy-ssh <mr.linux.0@protonmail.com>
2025-11-13 21:23:09 +0530

    fonts added, navbar layout added

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -3,9 +3,20 @@
 const Dashboard = () => {
     return (
         <>
-            <h1>
-                Dashboard
-            </h1>
+            <div className="nav">
+                <div className="nav-brand">
+                    <img src="" alt="" />
+                </div>
+                <div className="nav-focus-links">
+                    <a href="" className="focus-link">Learn</a>
+                    <a href="" className="focus-link">Inspire</a>
+                    <a href="" className="focus-link">Jobs</a>
+                    <a href="" className="focus-link">Profile</a>
+                </div>
+                <div className="nav-skill-search-box">
+                    <input type="text" className="search-box" />
+                </div>
+            </div>
         </>
     )
 }
